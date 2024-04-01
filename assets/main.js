541e898791b438b7f791e4540b19b25c52285d09
Ankit <foodifiz064@gmail.com>
2024-04-01 09:05:35 +0530

    optimise commit

--- assets/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#changeClassButton").click(function(){
-      $(".mobile-nav-menu").toggleClass("open-menu");
-    });
-
-    $("#closeClassButton").click(function(){
-      $(".mobile-nav-menu").toggleClass("open-menu");
-    });
-  });
